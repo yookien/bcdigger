ce398cd4d00d1ef6518d67a0dea7ad82afbe9a7f
yookien <yookien@gmail.com>
2018-07-20 19:49:16 +0800

    db sql

--- bcdigger/db_bcdigger.sql
+++ bcdigger/db_bcdigger.sql
@@ -6,7 +6,7 @@ drop table if exists bcd_admin;
 /*==============================================================*/
 create table bcd_admin
 (
-   id             int comment '主键id',
+   id             int AUTO_INCREMENT comment '主键id',
    store_id		  int comment '所属门店id',		
    name                 varchar(64) comment '名称',
    nickname             varchar(64) comment '昵称',
@@ -33,6 +33,158 @@ create table bcd_admin
 ALTER TABLE `bcd_admin` ADD UNIQUE INDEX `name` (`name`) ;
 alter table bcd_admin comment '管理员表';
 
+insert into bcd_admin(`name`,nickname,`password`,add_time,update_time,state,store_id)
+values
+('王朝文','王朝文','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('徐湘平','徐湘平','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('梁青敏','梁青敏','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('曾雅婷','曾雅婷','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('陈颖','陈颖','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('付芒','付芒','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('蔡茜城','蔡茜城','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('张大庄','张大庄','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('李萍','李萍','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('TEST','TEST','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,1),
+('NXHBBJ0001','北京奈雪西单大悦城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,58),
+('NXHBBJ0002','北京奈雪西直门凯德店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,59),
+('NXXNCD0002','成都奈雪万象城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,65),
+('TGXNCD0002','成都台盖锦华万达广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,183),
+('TGXNCD0001','成都台盖九方店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,182),
+('NXHNDG0001','东莞奈雪汇一城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,74),
+('TGHNDG0001','东莞台盖海德广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,188),
+('NXHNFS0001','佛山奈雪岭南天地店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,76),
+('NXHNFS0002','佛山奈雪中海环宇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,77),
+('TGHNFZ0001','福州台盖东二环泰禾广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,189),
+('NXHNGZ0003','广州奈雪花城汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,81),
+('NXHNGZ0007','广州奈雪基盛万科店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,85),
+('NXHNGZ0010','广州奈雪乐峰广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,88),
+('NXHNGZ0004','广州奈雪漫广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,82),
+('NXHNGZ0008','广州奈雪同和金铂广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,86),
+('NXHNGZ0001','广州奈雪万菱汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,79),
+('NXHNGZ0002','广州奈雪正佳广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,80),
+('NXHNGZ0005','广州奈雪珠影星光城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,83),
+('TGHNGZ0002','广州台盖k11店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,191),
+('TGHNGZ0001','广州台盖百信广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,190),
+('TGHNGZ0004','广州台盖基盛万科店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,193),
+('NXHDHZ0004','杭州奈雪滨江宝龙店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,94),
+('NXHDHZ0002','杭州奈雪大厦中央广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,92),
+('NXHDHZ0003','杭州奈雪湖滨银泰店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,93),
+('NXHDHZ0001','杭州奈雪万象城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,91),
+('NXHDHZ0005','杭州奈雪萧山万象汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,95),
+('TGHDHZ0001','杭州台盖中央广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,194),
+('NXHDNJ0001','南京奈雪德基广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,97),
+('NXHDNJ0004','南京奈雪东方福来德店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,100),
+('NXHDNJ0003','南京奈雪河西金鹰店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,99),
+('NXHDNJ0002','南京奈雪仙林万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,98),
+('TGHDNJ0003','南京台盖德基广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,199),
+('TGHDNJ0002','南京台盖仙林万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,198),
+('TGHNXM0002','厦门台盖中华城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,221),
+('NXHDSH0004','上海奈雪环宇荟店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,104),
+('NXHDSH0001','上海奈雪市百一店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,101),
+('NXHDSH0002','上海奈雪长风大悦城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,102),
+('TGHDSH0002','上海台盖广元西路店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,270),
+('TGHDSH0004','上海台盖国金中心店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,272),
+('TGHDSH0006','上海台盖静安大悦城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,205),
+('TGHDSH0003','上海台盖友谊南方商城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,271),
+('TGHDSH0001','上海台盖颛桥万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,269),
+('LSHNSZ0002','深圳梨山海岸城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,12),
+('LSHNSZ0001','深圳梨山天安云谷店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,11),
+('NXHNSZ0024','深圳奈雪KKmall店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,39),
+('NXHNSZ0027','深圳奈雪kkone店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,42),
+('NXHNSZ0011','深圳奈雪八号仓店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,26),
+('NXHNSZ0028','深圳奈雪布吉万象汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,43),
+('NXHNSZ0036','深圳奈雪大梅沙店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,51),
+('NXHNSZ0048','深圳奈雪福田cocopark店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,115),
+('NXHNSZ0005','深圳奈雪福田东海店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,20),
+('NXHNSZ0034','深圳奈雪福田马成时代广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,49),
+('NXHNSZ0001','深圳奈雪福田卓越店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,16),
+('NXHNSZ0013','深圳奈雪海岸城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,28),
+('NXHNSZ0010','深圳奈雪海雅缤纷城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,25),
+('NXHNSZ0009','深圳奈雪后海君尚店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,24),
+('NXHNSZ0017','深圳奈雪花园城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,32),
+('NXHNSZ0003','深圳奈雪华强北九方店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,18),
+('NXHNSZ0040','深圳奈雪华强北茂业店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,55),
+('NXHNSZ0014','深圳奈雪华润万象天地店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,29),
+('NXHNSZ0002','深圳奈雪欢乐海岸店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,17),
+('NXHNSZ0012','深圳奈雪汇一城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,27),
+('NXHNSZ0020','深圳奈雪金光华店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,35),
+('NXHNSZ0023','深圳奈雪科技生态园店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,38),
+('NXHNSZ0015','深圳奈雪来福士店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,30),
+('NXHNSZ0038','深圳奈雪龙岗海航城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,53),
+('NXHNSZ0026','深圳奈雪龙华九方店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,41),
+('NXHNSZ0019','深圳奈雪绿景虹湾店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,34),
+('NXHNSZ0008','深圳奈雪梅林卓悦汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,23),
+('NXHNSZ0006','深圳奈雪南山大冲1期店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,21),
+('NXHNSZ0018','深圳奈雪南山京基店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,33),
+('NXHNSZ0004','深圳奈雪南山茂业店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,19),
+('NXHNSZ0042','深圳奈雪南山太古城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,57),
+('NXHNSZ0025','深圳奈雪前海周大福店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,40),
+('NXHNSZ0030','深圳奈雪沙井京基店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,45),
+('NXHNSZ0043','深圳奈雪沙井天虹店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,110),
+('NXHNSZ0039','深圳奈雪深业上城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,54),
+('NXHNSZ0035','深圳奈雪天安云谷店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,50),
+('NXHNSZ0033','深圳奈雪万科龙城广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,48),
+('NXHNSZ0029','深圳奈雪万科天誉广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,44),
+('NXHNSZ0022','深圳奈雪新城市广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,37),
+('NXHNSZ0052','深圳奈雪雅宝cocopark店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,119),
+('NXHNSZ0016','深圳奈雪壹方城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,31),
+('NXHNSZ0007','深圳奈雪中心城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,22),
+('TGHNSZ0039','深圳台盖布吉万象汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,211),
+('TGHNSZ0035','深圳台盖创投大厦超级物种店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,207),
+('TGHNSZ0034','深圳台盖福田cocopark店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,181),
+('TGHNSZ0006','深圳台盖福田东海店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,153),
+('TGHNSZ0036','深圳台盖福田丰盛町店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,208),
+('TGHNSZ0027','深圳台盖观澜湖店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,174),
+('TGHNSZ0024','深圳台盖国创中心店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,171),
+('TGHNSZ0010','深圳台盖海岸城旗舰店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,157),
+('TGHNSZ0016','深圳台盖花园城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,163),
+('TGHNSZ0041','深圳台盖华南城奥特莱斯店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,213),
+('TGHNSZ0003','深圳台盖华强北九方店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,150),
+('TGHNSZ0029','深圳台盖华强茂业店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,176),
+('TGHNSZ0017','深圳台盖欢乐海岸店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,164),
+('TGHNSZ0023','深圳台盖皇庭广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,170),
+('TGHNSZ0012','深圳台盖汇一城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,159),
+('TGHNSZ0014','深圳台盖金光华店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,161),
+('TGHNSZ0022','深圳台盖科技生态园店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,169),
+('TGHNSZ0018','深圳台盖来福士店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,165),
+('TGHNSZ0004','深圳台盖乐淘里店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,151),
+('TGHNSZ0033','深圳台盖龙岗海航城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,180),
+('TGHNSZ0001','深圳台盖龙华cococity店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,148),
+('TGHNSZ0026','深圳台盖龙华九方店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,173),
+('TGHNSZ0015','深圳台盖绿景虹湾店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,162),
+('TGHNSZ0005','深圳台盖绿景香颂店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,152),
+('TGHNSZ0007','深圳台盖梅林卓悦汇店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,154),
+('TGHNSZ0020','深圳台盖民治天虹店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,167),
+('TGHNSZ0009','深圳台盖南山茂业店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,156),
+('TGHNSZ0032','深圳台盖天安云谷店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,179),
+('TGHNSZ0013','深圳台盖同泰广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,160),
+('TGHNSZ0030','深圳台盖万科天誉广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,177),
+('TGHNSZ0008','深圳台盖新沙天虹店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,155),
+('TGHNSZ0042','深圳台盖雅宝cocopark店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,214),
+('TGHNSZ0021','深圳台盖壹方城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,168),
+('TGHNSZ0025','深圳台盖益田假日店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,172),
+('TGHNSZ0019','深圳台盖中心城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,166),
+('XNHNSZ0007','深圳喜年贡茶八号仓店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,231),
+('XNHNSZ0012','深圳喜年贡茶大浪店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,236),
+('NXHZWH0003','武汉奈雪光谷K11店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,122),
+('NXHZWH0002','武汉奈雪汉街万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,121),
+('NXHZWH0005','武汉奈雪菱角湖万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,124),
+('NXHZWH0001','武汉奈雪新天地店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,120),
+('NXHZWH0004','武汉奈雪宜家荟聚店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,123),
+('TGHZWH0003','武汉台盖M+世纪都会广场店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,218),
+('TGHZWH0001','武汉台盖壹方购物中心店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,216),
+('NXXBXA0003','西安奈雪SKP店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,129),
+('NXXBXA0001','西安奈雪高新万达店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,127),
+('NXXBXA0002','西安奈雪益田假日店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,128),
+('TGXBXA0001','西安台盖赛格店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,219),
+('NXHZCS0002','长沙奈雪I city店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,72),
+('NXHZCS0001','长沙奈雪梅溪湖店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,71),
+('TGHZCS0001','长沙台盖国金中心店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,187),
+('NXXNCQ0002','重庆奈雪万象城店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,68),
+('NXXNCQ0003','重庆奈雪源著天街店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,69),
+('TGXNCQ0001','重庆台盖北城天街店','646826576E5AB3C65D9FC4327E8B100C',now(),now(),1,185);
+
+
 drop table if exists bcd_sys_menu;
 
 /*==============================================================*/
@@ -40,7 +192,7 @@ drop table if exists bcd_sys_menu;
 /*==============================================================*/
 create table bcd_sys_menu
 (
-   id              int comment '主键id',
+   id              int AUTO_INCREMENT comment '主键id',
    menu_name            varchar(64) comment '菜单名称',
    menu_url             varchar(255) comment '菜单url',
    menu_logo_url        varchar(255) comment '菜单图标url',
@@ -64,7 +216,7 @@ drop table if exists bcd_admin_role;
 /*==============================================================*/
 create table bcd_admin_role
 (
-   id              int comment '主键id',
+   id              int AUTO_INCREMENT comment '主键id',
    role_name            varchar(64) comment '角色名称',
    role_desc            varchar(255) comment '角色描述',
    state                int comment '状态（0失效，1有效）',
@@ -83,7 +235,7 @@ drop table if exists bcd_admin_role_ref;
 /*==============================================================*/
 create table bcd_admin_role_ref
 (
-   id        int comment '主键id',
+   id        int AUTO_INCREMENT comment '主键id',
    admin_id             int comment '管理员id',
    role_ids             varchar(255) comment '角色ids（用,分隔）',
    state                int comment '状态（0失效，1有效）',
@@ -102,7 +254,7 @@ drop table if exists bcd_role_menu_ref;
 /*==============================================================*/
 create table bcd_role_menu_ref
 (
-   id         int comment '主键id',
+   id         int AUTO_INCREMENT comment '主键id',
    role_id              int comment '角色id',
    menu_ids             varchar(255) comment '菜单ids(用,分隔)',
    state                int comment '状态',
@@ -121,7 +273,7 @@ drop table if exists bcd_admin_operate_log;
 /*==============================================================*/
 create table bcd_admin_operate_log
 (
-   id               int comment '日志主键',
+   id               int AUTO_INCREMENT comment '日志主键',
    admin_id             int comment '操作员id',
    admin_name           varchar(64) comment '操作员姓名',
    operator_type        int comment '操作类型（1增加，2修改，3删除，4查询）',
@@ -145,7 +297,7 @@ drop table if exists bcd_post_category;
 /*==============================================================*/
 create table bcd_post_category
 (
-   id                   int not null comment '主键id',
+   id                   int not null AUTO_INCREMENT comment '主键id',
    name                 varchar(255) comment '分类名称',
    all_name             varchar(255) comment '分类全称',
    display_order        int comment '显示顺序',
@@ -169,7 +321,7 @@ drop table if exists bcd_post;
 /*==============================================================*/
 create table bcd_post
 (
-   id                   int not null comment '主键id',
+   id                   int not null AUTO_INCREMENT comment '主键id',
    post_author_id       int comment '文章作者id',
    post_source          varchar(255) comment '文章来源',
    post_date            bigint comment '发布时间',
@@ -202,7 +354,7 @@ drop table if exists bcd_post_tag;
 /*==============================================================*/
 create table bcd_post_tag
 (
-   id                   int not null comment '主键id',
+   id                   int not null AUTO_INCREMENT comment '主键id',
    tag_name             varchar(64) comment '名称',
    state                int comment '状态',
    display_order        int comment '显示位置',
@@ -219,7 +371,7 @@ drop table if exists bcd_users;
 /*==============================================================*/
 create table bcd_users
 (
-   id                   int not null comment '主键id',
+   id                   int not null AUTO_INCREMENT comment '主键id',
    user_name            varchar(255) comment '用户名',
    user_passwd          varchar(255) comment '用户密码',
    user_nicename        varchar(255) comment '用户昵称',
